d00ae12ef4d4649b18069742a17245de16b13877
Jim Hayes <jhayes@sdsc.edu>
2014-09-26 13:53:41 -0700

    Modulefile rpm build restructuring.
    Adopt/adapt Rocks approach to copying version from corresponding s/w package.
    Move all variant definitions into version.mk so that identical modulefile
    Makefile will work across rolls.

--- src/beagle-modules/version.mk
+++ src/beagle-modules/version.mk
@@ -1,5 +1,11 @@
-NAME       = beagle-modules
-VERSION    = 1
-RELEASE    = 0
+NAME        = beagle-modules
+RELEASE     = 1
+PKGROOT     = /opt/modulefiles/applications/beagle
 
-RPM.EXTRAS = AutoReq:No
+VERSION_SRC = $(REDHAT.ROOT)/src/beagle_1.1/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+ADD_VERSION = 2.1
+
+RPM.EXTRAS  = AutoReq:No
