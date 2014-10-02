f036236113fa1a57cd022fcf6a5c12eeb7108857
Jerry Greenberg <jpg@gcn-20-88.sdsc.edu>
2014-10-02 16:03:10 -0700

    updated release number

--- src/beagle-modules/version.mk
+++ src/beagle-modules/version.mk
@@ -1,4 +1,12 @@
-NAME    = beagle-modules
-VERSION = 2.1
-RELEASE = 3
-RPM.EXTRAS         = "AutoReq: no"
+PACKAGE     = beagle
+CATEGORY    = applications
+
+NAME        = $(PACKAGE)-modules
+RELEASE     = 3
+PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
+
+VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
+VERSION_INC = version.inc
+include $(VERSION_INC)
+
+RPM.EXTRAS  = AutoReq:No
